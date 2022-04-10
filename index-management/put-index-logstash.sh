1f55811cc44832744bb5240f9e406935ab09326e
Chanasit <kai.chr160@gmail.com>
2022-04-11 00:28:07 +0700

    tuning fleunt bit

--- index-management/put-index-logstash.sh
+++ index-management/put-index-logstash.sh
@@ -8,45 +8,12 @@ curl -X PUT "localhost:9200/_index_template/logstash?pretty" -H 'Content-Type: a
           "name": "7-days-default",
           "rollover_alias": "logstash"
         },
-        "number_of_replicas": "1"
+        "number_of_replicas": "2"
       }
     },
     "mappings": {
       "dynamic": "true",
-      "dynamic_date_formats": [
-        "strict_date_optional_time",
-        "yyyy/MM/dd HH:mm:ss Z||yyyy/MM/dd Z"
-      ],
-      "dynamic_templates": [
-      ],
-      "date_detection": true,
-      "numeric_detection": false,
-      "properties": {
-        "@timestamp": {
-          "type": "date"
-        },
-        "data_stream": {
-          "properties": {
-            "dataset": {
-              "type": "constant_keyword"
-            },
-            "namespace": {
-              "type": "constant_keyword"
-            }
-          }
-        },
-        "ecs": {
-          "properties": {
-            "version": {
-              "type": "keyword",
-              "ignore_above": 1024
-            }
-          }
-        },
-        "host": {
-          "type": "object"
-        }
-      }
+      "properties": {}
     },
     "aliases": {
       "logstash": {}
